9c924268e71e065affb7964ba3cfa7512140ab9d
shedaniel <daniel@shedaniel.me>
2021-12-14 01:15:32 +0800

    Stub plugin for now
    Signed-off-by: shedaniel <daniel@shedaniel.me>

---
+++ src/main/java/dev/architectury/pack200/java/Pack200Plugin.java
@@ -0,0 +1,10 @@
+package dev.architectury.pack200.java;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.plugins.PluginAware;
+
+public class Pack200Plugin implements Plugin<PluginAware> {
+	@Override
+	public void apply(PluginAware pluginAware) {
+	}
+}
